fa13129cbd36d0585c073bf2237226e4f29e3299
Arthur Coutinho <arthurtelescoutinho@gmail.com>
2024-03-01 12:14:26 -0300

    feat: board and piece created

---
+++ src/br/newtonpaiva/board/Position.java
@@ -0,0 +1,39 @@
+package br.newtonpaiva.board;
+
+public class Position {
+    private Integer row;
+    private Integer column;
+
+    public Position() {
+    }
+
+    public Position(Integer row, Integer column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public void setValue(int row, int column){
+
+    }
+
+    public Integer getRow() {
+        return row;
+    }
+
+    public void setRow(Integer row) {
+        this.row = row;
+    }
+
+    public Integer getColumn() {
+        return column;
+    }
+
+    public void setColumn(Integer column) {
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        return  row + ", " + column;
+    }
+}
--- src/br/newtonpaiva/ui/Main.java
+++ src/br/newtonpaiva/ui/Main.java
@@ -1,8 +1,12 @@
 package br.newtonpaiva.ui;
 
+import br.newtonpaiva.board.Board;
+import br.newtonpaiva.board.Position;
+
 public class Main {
     public static void main(String[] args) {
+        Board board = new Board(8,8);
 
-        System.out.println("Hello world!");
+        System.out.println(board);
     }
 }
