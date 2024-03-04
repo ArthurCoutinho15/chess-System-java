0dfcd3cc2704a8a57215912133b789a2a8a940f6
Arthur Coutinho <arthurtelescoutinho@gmail.com>
2024-03-04 13:40:15 -0300

    feat: chess exception and chess position

--- src/br/newtonpaiva/chess/ChessMatch.java
+++ src/br/newtonpaiva/chess/ChessMatch.java
@@ -23,10 +23,14 @@ public class ChessMatch {
         return mat;
     }
 
+    private void placeNewPiece(char column, int row, ChessPiece piece){
+        board.placePieces(piece, new ChessPosition(column, row).toPosition());
+    }
+
     private void initialSetup(){
-        board.placePieces(new Rook(board, Color.WHITE), new Position(0,0));
-        board.placePieces(new King(board, Color.BLACK), new Position(0,4));
-        board.placePieces(new King(board, Color.WHITE), new Position(7,4));
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
     }
 
 }
